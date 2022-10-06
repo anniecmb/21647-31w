a6df5df00fe5a3add6c7bf66ece76d142bd04f0e
Annie-Claude Martin-Brochu <102246318+anniecmb@users.noreply.github.com>
2022-10-06 10:30:49 -0400

    cours8 - Modification des noms de classes site__header, site__branding, site__title, site_description

--- header.php
+++ header.php
@@ -23,16 +23,16 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<h1 class="site-title">
+	<header id="masthead" class="site__header">
+		<div class="site__branding">
+			<h1 class="site__title">
                 <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
             </h1>
         <?php
 			$underscore_description = get_bloginfo( 'description', 'display' );
 			if ( $underscore_description || is_customize_preview() ) :
 		?>
-				<p class="site-description"><?php echo $underscore_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+				<p class="site__description"><?php echo $underscore_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
@@ -43,3 +43,5 @@
 			"container_class" => "menu__primaire")); ?>
 
 	</header><!-- #masthead -->
+
+	
